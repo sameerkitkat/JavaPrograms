f9821773a6fa5686c5c2a665856e39f2161d7fc9
Sameer Kulkarni <sameerkitkat4.4@gmail.com>
2020-04-22 00:32:48 +0530

    Adding program for longest non repeating substring

---
+++ LongestSubstringWithoutRepeatingCharacters/src/LongestSubstringNonrepeating.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubstringNonrepeating {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        LongestSubstringNonrepeating longest = new LongestSubstringNonrepeating();
+        int num = longest.lengthOfLongestSubstring(s);
+        System.out.println(num);
+    }
+}
